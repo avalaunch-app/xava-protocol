dee7926503907d17da98cb1dcc62c45efb7916ab
avalaunch-dev <development@avalaunch.app>
2021-05-07 21:27:32 +0200

    Implement initial portion getting unlocked immediately.

--- contracts/ParticipationVesting.sol
+++ contracts/ParticipationVesting.sol
@@ -10,9 +10,11 @@ contract ParticipationVesting  {
     using SafeMath for *;
 
     struct Participation {
-        uint256 participationAmount;
+        uint256 initialPortion;
+        uint256 vestedAmount;
         uint256 amountPerPortion;
-        bool [] isPortionWithdrawn;
+        bool initialPortionWithdrawn;
+        bool [] isVestedPortionWithdrawn;
     }
 
     IERC20 public token;
@@ -56,20 +58,24 @@ contract ParticipationVesting  {
         address participant,
         uint participationAmount
     )
-    public
+    external
     onlyAdmin
     {
         require(hasParticipated[participant] == false, "User already registered as participant.");
+        uint initialPortionAmount = participationAmount.mul(20).div(100);
+        // Vested 80%
+        uint vestedAmount = participationAmount.sub(initialPortionAmount);
         // Compute amount per portion
-        uint portionAmount = participationAmount.div(numberOfPortions);
-
+        uint portionAmount = vestedAmount.div(numberOfPortions);
         bool[] memory isPortionWithdrawn = new bool[](numberOfPortions);
 
         // Create new participation object
         Participation memory p = Participation({
-            participationAmount: participationAmount,
+            initialPortion: initialPortionAmount,
+            vestedAmount: vestedAmount,
             amountPerPortion: portionAmount,
-            isPortionWithdrawn: isPortionWithdrawn
+            initialPortionWithdrawn: false,
+            isVestedPortionWithdrawn: isPortionWithdrawn
         });
 
         // Map user and his participation
@@ -78,26 +84,33 @@ contract ParticipationVesting  {
         hasParticipated[participant] = true;
     }
 
+
     // User will always withdraw everything available
     function withdraw()
-    public
+    external
     {
         address user = msg.sender;
         require(hasParticipated[user] == true, "Withdraw: User is not a participant.");
 
         Participation storage p = addressToParticipation[user];
+
         uint256 totalToWithdraw = 0;
+
+        // Initial portion can be unlocked whenever
+        if(p.initialPortionWithdrawn == false) {
+            totalToWithdraw = totalToWithdraw.add(p.initialPortion);
+        }
 
         uint i = 0;
 
         while (isPortionUnlocked(i) == true && i < distributionDates.length) {
             // If portion is not withdrawn
-            if(!p.isPortionWithdrawn[i]) {
+            if(!p.isVestedPortionWithdrawn[i]) {
                 // Add this portion to withdraw amount
                 totalToWithdraw = totalToWithdraw.add(p.amountPerPortion);
 
                 // Mark portion as withdrawn
-                p.isPortionWithdrawn[i] = true;
+                p.isVestedPortionWithdrawn[i] = true;
             }
             // Increment counter
             i++;
