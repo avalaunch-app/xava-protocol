63da321a586698861fcf915e612ea978af8d8c82
avalaunch-dev <nikola@avalaunch.app>
2021-05-14 10:22:07 +0200

    Add deploy_vesting_private.js and deploy_vesting_private.js

--- scripts/deploy_vesting_private.js
+++ scripts/deploy_vesting_private.js
@@ -21,17 +21,8 @@ async function main() {
     );
 
     await participationVestingContract.deployed();
-    console.log("Participation Vesting Seed contract deployed to: ", participationVestingContract.address);
-    saveContractAddress(hre.network.name, 'ParticipationVestingSeed', participationVestingContract.address);
-
-    let token = await hre.ethers.getContractAt('XavaToken', getSavedContractAddresses()[hre.network.name]["XavaToken"]);
-    await token.transfer(participationVestingContract.address, "60000000000000000000000");
-    console.log('Transfer done');
-
-    await participationVestingContract.registerParticipants(
-        ['0xf3B39c28bF4c5c13346eEFa8F90e88B78A610381','0x3EC7eF9B96a36faa0c0949a2ba804f60D12593Dd'],
-        ['2000000000000000000000','4000000000000000000000']
-    );
+    console.log("Participation Vesting Private contract deployed to: ", participationVestingContract.address);
+    saveContractAddress(hre.network.name, 'ParticipationVestingPrivate', participationVestingContract.address);
 }
 
 
