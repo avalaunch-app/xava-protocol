cc9b9dfca27b5e40a9752648aa9a36b4c85c615c
avalaunch-dev <nikola@avalaunch.app>
2021-05-18 21:47:06 +0200

    Remove non used EnumerableSet

--- contracts/FarmingXava.sol
+++ contracts/FarmingXava.sol
@@ -3,7 +3,6 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
