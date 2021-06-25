096e2a29b8473cf271a3f41419e57828aec019bd
avalaunch-dev <nikola@avalaunch.app>
2021-06-25 16:21:05 +0200

    Add require statement messages.

--- contracts/sales/SalesFactory.sol
+++ contracts/sales/SalesFactory.sol
@@ -43,9 +43,10 @@ contract SalesFactory {
 
     // Function to set owner and token for the sale
     function setSaleOwnerAndToken(address saleOwner, address saleToken) external {
-        require(isSaleCreatedThroughFactory[msg.sender] == true);
-        require(saleOwnerToSale[saleOwner] == address(0));
-        require(tokenToSale[saleToken] == address(0));
+        require(isSaleCreatedThroughFactory[msg.sender] == true, "setSaleOwnerAndToken: Contract not eligible.");
+        require(saleOwnerToSale[saleOwner] == address(0), "Sale owner already set.");
+        require(tokenToSale[saleToken] == address(0), "Sale token already set.");
+
         // Set owner of the sale.
         saleOwnerToSale[saleOwner] = msg.sender;
         // Set token to sale
