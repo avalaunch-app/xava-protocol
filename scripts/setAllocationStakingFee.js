38f687e9509800c5746cd683a938a9e9254ea8dc
avalaunch-dev <nikola@avalaunch.app>
2021-07-23 10:11:09 +0200

    Add script to setAllocation

---
+++ scripts/setAllocationStakingFee.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+const { saveContractAddress, getSavedContractAddresses } = require('./utils')
+const { ethers, web3 } = hre
+
+async function getCurrentBlockTimestamp() {
+    return (await ethers.provider.getBlock('latest')).timestamp;
+}
+
+async function main() {
+
+    const contracts = getSavedContractAddresses()[hre.network.name];
+
+    const allocationStaking = await hre.ethers.getContractAt('AllocationStaking', contracts['AllocationStaking']);
+    await allocationStaking.setDepositFee(20000000, 1000000000);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
