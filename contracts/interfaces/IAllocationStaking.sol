8fa3aa7e2a9a5115ec6fadd92820a0dba9573337
avalaunch-dev <nikola@avalaunch.app>
2021-08-09 16:00:47 +0200

    Fix interface missmatch.

--- contracts/interfaces/IAllocationStaking.sol
+++ contracts/interfaces/IAllocationStaking.sol
@@ -2,6 +2,6 @@
 pragma solidity 0.6.12;
 
 interface IAllocationStaking {
-    function redistributeXava(uint256 _pid, address _user, uint256 _amountToBurn, uint256 minimalStakeAmount) external;
+    function redistributeXava(uint256 _pid, address _user, uint256 _amountToBurn) external;
     function deposited(uint256 _pid, address _user) external view returns (uint256);
 }
