d1adb8ade7dfe85fb46fa7637315e0913032f764
avalaunch-dev <nikola@avalaunch.app>
2021-08-17 20:21:58 +0200

    Fix migrations.

--- scripts/deploy_staging.js
+++ scripts/deploy_staging.js
@@ -1,6 +1,5 @@
 const hre = require("hardhat");
 const { saveContractAddress } = require('./utils')
-const { ethers, web3 } = hre
 const config = require('./config.json');
 
 async function getCurrentBlockTimestamp() {
@@ -11,25 +10,24 @@ async function main() {
 
     const c = config[hre.network.name];
 
-
     const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 
-    const XavaToken = await hre.ethers.getContractFactory("XavaToken");
+    const XavaToken = await ethers.getContractFactory("XavaToken");
     const token = await XavaToken.deploy(c.token.tokenName, c.token.symbol, c.token.totalSupply, c.token.decimals);
     await token.deployed();
     console.log("Xava MOCK Token deployed to: ", token.address);
     saveContractAddress(hre.network.name, "MOCK-XAVA", token.address);
 
 
-    const Admin = await hre.ethers.getContractFactory("Admin");
+    const Admin = await ethers.getContractFactory("Admin");
     const admin = await Admin.deploy(c.admins);
     await admin.deployed();
     console.log("Admin contract deployed to: ", admin.address);
     saveContractAddress(hre.network.name, "Admin", admin.address);
 
 
-    const SalesFactory = await hre.ethers.getContractFactory("SalesFactory");
+    const SalesFactory = await ethers.getContractFactory("SalesFactory");
     const salesFactory = await SalesFactory.deploy(admin.address, ZERO_ADDRESS);
     await salesFactory.deployed();
     saveContractAddress(hre.network.name, "SalesFactory", salesFactory.address);
@@ -38,7 +36,8 @@ async function main() {
 
     const currentTimestamp = await getCurrentBlockTimestamp();
 
-    const AllocationStaking = await hre.ethers.getContractFactory("AllocationStaking");
+    const AllocationStaking = await ethers.getContractFactory("AllocationStaking");
+
     const allocationStaking = await upgrades.deployProxy(AllocationStaking, [
             token.address,
             ethers.utils.parseEther(c.allocationStakingRPS),
@@ -51,7 +50,7 @@ async function main() {
     console.log('AllocationStaking Proxy deployed to:', allocationStaking.address);
     saveContractAddress(hre.network.name, 'AllocationStaking', allocationStaking.address);
 
-    
+
     await salesFactory.setAllocationStaking(allocationStaking.address);
 
     const totalRewards = ethers.utils.parseEther(c.initialRewardsAllocationStaking);
