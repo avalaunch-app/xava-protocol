788f56b63a855e5a6023888f2fe1cb69a96e7988
avalaunch-dev <nikola@avalaunch.app>
2021-10-12 23:05:35 +0200

    Add deploy_airdropHCT.js script.

---
+++ scripts/deploy_airdropHCT.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+const { saveContractAddress, getSavedContractAddresses} = require('./utils')
+
+async function main() {
+    const contracts = getSavedContractAddresses()[hre.network.name];
+
+    const AirdropHCT = await hre.ethers.getContractFactory("AirdropHCT");
+    const hctToken = '0x45c13620b55c35a5f539d26e88247011eb10fdbd';
+    const airdropContract = await AirdropHCT.deploy(hctToken, contracts['Admin']);
+    await airdropContract.deployed();
+
+
+    console.log("AirdropHCT contract is deployed to: ", airdropContract.address);
+    saveContractAddress(hre.network.name, "AirdropHCT", airdropContract.address);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
