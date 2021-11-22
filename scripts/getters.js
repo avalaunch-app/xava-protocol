cd07fbeb3112dfc79b61ed396425eab52cc84d1f
0xCrypto <nikola@avalaunch.app>
2021-11-22 23:41:56 +0100

    Set post sale config and length

--- scripts/getters.js
+++ scripts/getters.js
@@ -14,17 +14,8 @@ async function main() {
         contracts['AllocationStakingProxy']
     );
 
-    const unlockTime = await getCurrentBlockTimestamp();
-
-    const stake = await allocationStaking.pending(0, '0xE8E6959a29bB94cB1080DE4257417E6f22AB3AE2');
-    console.log(stake);
-
-    const endDate = await allocationStaking.setTokensUnlockAtForUser(
-        '0xE8E6959a29bB94cB1080DE4257417E6f22AB3AE2',
-        0,
-        unlockTime - 1800
-    );
-
+    const payload = await allocationStaking.depositFeePrecision();
+    console.log(payload.toString());
 }
 
 
