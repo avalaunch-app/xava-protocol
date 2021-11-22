4fb90e364a0417e88077b0174d2b2950652bab46
0xCrypto <nikola@avalaunch.app>
2021-11-22 23:29:47 +0100

    scripts

---
+++ scripts/getters.js
@@ -0,0 +1,36 @@
+// scripts/upgrade-box.js
+const { ethers, upgrades } = require("hardhat");
+const { getSavedContractAddresses } = require('./utils')
+const hre = require("hardhat");
+
+async function getCurrentBlockTimestamp() {
+    return (await ethers.provider.getBlock('latest')).timestamp;
+}
+
+async function main() {
+    const contracts = getSavedContractAddresses()[hre.network.name];
+    const allocationStaking = await hre.ethers.getContractAt(
+        'AllocationStaking',
+        contracts['AllocationStakingProxy']
+    );
+
+    const unlockTime = await getCurrentBlockTimestamp();
+
+    const stake = await allocationStaking.pending(0, '0xE8E6959a29bB94cB1080DE4257417E6f22AB3AE2');
+    console.log(stake);
+
+    const endDate = await allocationStaking.setTokensUnlockAtForUser(
+        '0xE8E6959a29bB94cB1080DE4257417E6f22AB3AE2',
+        0,
+        unlockTime - 1800
+    );
+
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
