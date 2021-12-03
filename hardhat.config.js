815087ac0d47b267cfb371eea7c0f1cf9e5d18b7
R0x0ted <marko@avalaunch.app>
2021-12-03 11:53:38 +0100

    Add testPK to hardhat.config.js.

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,6 +15,8 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+const testPK = "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -42,14 +44,14 @@ module.exports = {
       url: 'https://api.avax-test.network/ext/bc/C/rpc',
       gasPrice: 225000000000,
       chainId: 43113,
-      accounts: [process.env.PK]
+      accounts: [process.env.PK || testPK]
     },
     mainnet: {
       url: 'https://api.avax.network/ext/bc/C/rpc',
       gasPrice: 225000000000,
       chainId: 43114,
       timeout: 900000000,
-      accounts: [process.env.PK]
+      accounts: [process.env.PK || testPK]
     },
     local: {
       url: 'http://localhost:8545',
