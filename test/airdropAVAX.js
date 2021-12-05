15251a8a98a6ce5c6a5ef745af7b3ba4b554bbea
R0x0ted <marko@avalaunch.app>
2021-12-05 09:15:38 +0100

    test/airdropAVAX.js remove web3.

--- test/airdropAVAX.js
+++ test/airdropAVAX.js
@@ -1,8 +1,6 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const { signTokenWithdrawal } = require("./helpers/signatures.js");
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545/'));
 
 const DEPLOYER_PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 const BAD_DEPLOYER_PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff89";
