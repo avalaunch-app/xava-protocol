87a7aa21fbccc7bf6ae6cf2e80a7363a020e18fe
R0x0ted <marko@avalaunch.app>
2022-01-31 00:39:34 +0100

    Remove unnecessary data type conversion.

--- contracts/sales/AvalaunchSale.sol
+++ contracts/sales/AvalaunchSale.sol
@@ -1070,8 +1070,8 @@ contract AvalaunchSale is Initializable {
 
     /// @notice     Function to get sale.token symbol and parse as bytes32
     function getTokenSymbolBytes32() internal view returns (bytes32 _symbol) {
-        // get token symbol and convert it to bytes memory
-        bytes memory _bytes = bytes(IERC20Metadata(address(sale.token)).symbol());
+        // get token symbol as string memory
+        string memory _bytes = IERC20Metadata(address(sale.token)).symbol();
         // parse token symbol to bytes32 format - to fit dexalot function interface
         assembly {
             _symbol := mload(add(_bytes, 32))
