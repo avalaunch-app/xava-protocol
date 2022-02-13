8344aa196bff903f6fea1f574cd5545fc9b0c1fb
R0x0ted <marko@avalaunch.app>
2022-02-13 23:00:00 +0100

    Add new sale implementation setter script.

---
+++ scripts/setters/set_new_sale_implementation.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+const { getSavedContractAddresses, saveContractAddress} = require('../utils');
+const { ethers } = hre;
+
+async function main() {
+    const contracts = getSavedContractAddresses()[hre.network.name];
+
+    const saleFactory = await ethers.getContractFactory("AvalaunchSale");
+    const saleImplementation = await saleFactory.deploy();
+    await saleImplementation.deployed();
+
+    saveContractAddress(hre.network.name, "Sale-Implementation", saleImplementation.address);
+    console.log('Sale implementation deployed to: ', saleImplementation.address);
+
+    const salesFactory = await ethers.getContractAt("SalesFactory", contracts['SalesFactory']);
+    await salesFactory.setImplementation(saleImplementation.address);
+    console.log('Sale implementation set on SalesFactory!');
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
