6d71bce803fde9cea8ddea84cd7a337c12a2b711
nftgeek <john@nftmall.io>
2022-03-20 03:52:51 +0000

    chore: remove unused function from IAdmin

--- contracts/interfaces/IAdmin.sol
+++ contracts/interfaces/IAdmin.sol
@@ -3,9 +3,4 @@ pragma solidity 0.6.12;
 
 interface IAdmin {
     function isAdmin(address user) external view returns (bool);
-
-    function verifySignature(bytes32 _hash, bytes calldata _signature)
-        external
-        view
-        returns (bool);
 }
