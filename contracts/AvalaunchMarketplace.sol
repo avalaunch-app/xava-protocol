6f324ce6fe3e4c2e38f6a634fa5fecb9cd062ec4
R0x0ted <marko@avalaunch.app>
2022-08-26 15:34:33 +0200

    wip

--- contracts/AvalaunchMarketplace.sol
+++ contracts/AvalaunchMarketplace.sol
@@ -82,29 +82,43 @@ contract AvalaunchMarketplace is Initializable {
 
     /**
      * @notice Function to buy portions from market
-     */
+     * @param sale is Avalaunch sale where portions are from
+	 * @param owner is account which owns the portions
+	 * @param sigExpTimestamp is signature expiration timestamp
+	 * @param portions is array of portion ids function caller wants to buy
+	 * @param prices is array of price values for portions
+	 * @param signature is admin signed data hash which confirms validity of action
+	 */
     function buyPortions(
         address sale,
         address owner,
+		uint256 sigExpTimestamp,
         uint256[] calldata portions,
         uint256[] calldata prices,
         bytes calldata signature
     ) external payable {
         require(officialSales[sale], "Invalid sale address.");
-        require(
+        // Make sure provided signature is signed by admin and containing valid data
+		require(
             admin.isAdmin(
-                keccak256(abi.encodePacked(owner, sale, portions, prices)).toEthSignedMessageHash().recover(signature)
+                keccak256(abi.encodePacked(owner, sale, portions, prices, sigExpTimestamp)).toEthSignedMessageHash().recover(signature)
             ),
 			"Invalid signature provided."
 		);
+		// Make sure signature is used in a valid timeframe
+		require(block.timestamp <= sigExpTimestamp, "Signature expired.");
 		// Mark portions as sold on sale contract
         IAvalaunchSaleV2(sale).transferPortions(owner, msg.sender, portions);
         uint256 total;
+		// Compute total amount to be paid for selected portions
         for(uint i = 0; i < portions.length; i++) {
             uint256 portionId = portions[i];
-            require(listedUserPortionsPerSale[owner][sale][portionId] == true, "Portion not buyable.");
+			// Make sure portion is for sale
+            require(listedUserPortionsPerSale[owner][sale][portionId] == true, "Portion not for sale.");
             total = total.add(prices[i]);
+			// Mark portion as 'not for sale'
             listedUserPortionsPerSale[owner][sale][portionId] = false;
+			// Trigger relevant event
             emit PortionSold(owner, msg.sender, sale, portionId, prices[i]);
         }
         require(msg.value == total, "Invalid AVAX amount sent.");
