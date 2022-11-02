e549eff6067ea106270f675076cefdb82d7d91b3
R0x0ted <marko@avalaunch.app>
2022-11-02 13:25:05 +0100

    add upgrade scripts

---
+++ scripts/upgrade/upgradeMarketplace.js
@@ -0,0 +1,31 @@
+// scripts/upgrade-box.js
+const { ethers } = require("hardhat");
+const { getSavedContractAddresses, getSavedProxyABI} = require('../utils')
+const hre = require("hardhat");
+
+async function main() {
+    const contracts = getSavedContractAddresses()[hre.network.name];
+    const proxyAdminAbi = getSavedProxyABI()['ProxyAdmin'];
+
+    const proxyAdmin = await hre.ethers.getContractAt(proxyAdminAbi, contracts['ProxyAdmin']);
+
+    const marketplaceProxy = contracts["AvalaunchMarketplaceProxy"];
+    console.log("Proxy:", marketplaceProxy);
+
+    const MarketplaceFactory = await ethers.getContractFactory("AvalaunchMarketplace");
+    const marketplaceImplementation = await MarketplaceFactory.deploy();
+    await marketplaceImplementation.deployed();
+
+    console.log("New Implementation:", marketplaceImplementation.address);
+
+    await proxyAdmin.upgrade(marketplaceProxy, marketplaceImplementation.address);
+    console.log("Marketplace contract upgraded");
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
